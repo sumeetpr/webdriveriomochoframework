d87028fb71323903a7e15bc1c56ec34ff8dfee4e
sumeet <sumeet.raikwar@gmail.com>
2023-03-07 19:39:20 +0530

    changes

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -8,7 +8,7 @@ describe('My Login application', () => {
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toContain(
-            'You logged into a secure area');
+            'You logged into a secure area!');
     });
 });
 
